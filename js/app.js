745809aa01e94df522b44d553306b7d670a789f7
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2023-11-03 15:30:16 -0300

    comentarios en todas las funciones

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
-// Apuntar a elementos del HTML
-const carsContainer = document.querySelector(".carsContainer");
-const selectBrand = document.querySelector("#brand");
-const selectYear = document.querySelector("#year");
-const selectModel = document.querySelector("#model");
+// ejecutar funcion cargar automoviles
+cargarAutomoviles("https://ha-front-api-proyecto-final.vercel.app/cars");
+// ejecutar funcion cargar años
+cargarAños(1900, 2023);
+// ejecutar funcion cargar marcas
+cargarMarcasyModelos();
 
-function cargarAutomoviles() {
-  fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
+/*
+ * cargarAutomoviles - Extrae informacion de la API para crear "carItems" e insertarlos en #CarContainer
+ * @param{apiURL} - url de la api para extraer informacions
+ */
+function cargarAutomoviles(apiURL) {
+  fetch(apiURL)
     .then(function (res) {
       return res.json();
     })
     .then(function (cars) {
+      const carsContainer = document.querySelector("#carsContainer");
+      carsContainer.innerHTML = "";
       for (const car of cars) {
         car.isNew = car.status
           ? `<span class="badge bg-warning position-absolute mt-3 mx-3">New</span>`
@@ -23,7 +30,8 @@ function cargarAutomoviles() {
         }
         carsContainer.insertAdjacentHTML(
           "beforeend",
-          `<div class="row mb-3">
+          `<div id="carItem">
+          <div class="row mb-3">
             <div class="col-12 col-lg-4">
               <div class="position-relative h-100">
                 ${car.isNew}
@@ -49,6 +57,7 @@ function cargarAutomoviles() {
             </button></div>
             </div>
           </div>
+          </div>
          <hr>`
         );
       }
@@ -57,42 +66,61 @@ function cargarAutomoviles() {
       console.error(err);
     });
 }
-// ejecutar funcion cargar automoviles
-cargarAutomoviles();
 
-// Cargar valores del filtro
-function cargarAnios() {
-  for (let year = 1900; year <= 2023; year++) {
+/*
+ * cargarAños - Inserta dentro del Select con id="year" Options con todos los años entre param{desde} y param{hasta}
+ * @param{desde} - Numero que representa un año
+ * @param {hasta} - Numero que representa un año posterior a param{desde}
+ */
+function cargarAños(desde, hasta) {
+  if (desde > hasta) {
+    return;
+  }
+
+  const selectYear = document.querySelector("#year");
+  for (let year = desde; year <= hasta; year++) {
     const option = document.createElement("option");
     option.value = year;
     option.text = year;
     selectYear.appendChild(option);
+    //  insertToSelector(selectYear, [year])
   }
 }
 
-// ejecutar funcion cargar años
-cargarAnios();
+/*
+ * cargarMarcasyModelos - Extrae informacion de la API para crear e insertar Options dentro del Select con id="brand" y
+ * extrae informacion de la API dependiendo el brand que el usuario elija para crear Options dentro del select con id="model"
+ */
+function cargarMarcasyModelos() {
+  fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (brands) {
+      const selectBrand = document.querySelector("#brand");
+      const selectModel = document.querySelector("#model");
 
-fetch("https://ha-front-api-proyecto-final.vercel.app/brands")
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (brands) {
-    insertToSelector(selectBrand,brands)
-    selectBrand.addEventListener("change", (e) => {
-      fetch(
-        `https://ha-front-api-proyecto-final.vercel.app/models?brand=${e.target.value}`
-      )
-        .then((res) => res.json())
-        .then((models) => {
-          insertToSelector(selectModel, models);
-        });
+      insertToSelector(selectBrand, brands);
+      selectBrand.addEventListener("change", (e) => {
+        fetch(
+          `https://ha-front-api-proyecto-final.vercel.app/models?brand=${e.target.value}`
+        )
+          .then((res) => res.json())
+          .then((models) => {
+            insertToSelector(selectModel, models);
+          });
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
     });
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
+}
 
+/*
+ * insertToSelector - Elimina todas las option de un Selector (menos la primera), luego crea e inserta nuevas option con los datos ingresados en la lista
+ * @param{selector} - Objeto que hace referencia a una etiqueta select del HTML
+ * @param{optionList} - Array de strings
+ */
 function insertToSelector(selector, optionList) {
   let firstOption = selector.firstElementChild;
   selector.innerHTML = "";
@@ -104,6 +132,7 @@ function insertToSelector(selector, optionList) {
     selector.appendChild(newOption);
   }
 }
+
 /*
 // Llama a la función para cargar los automóviles cuando se carga la página.
 
